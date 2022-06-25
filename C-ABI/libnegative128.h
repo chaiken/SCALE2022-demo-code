88bbe516c46c072efae0a6fc619db547756df853
Alison Chaiken <alison@she-devel.com>
2022-06-25 08:57:31 -0700

    add example with library-header mismatch

---
+++ C-ABI/libnegative128.h
@@ -0,0 +1,4 @@
+#ifndef IS_NEGATIVE
+#define IS_NEGATIVE
+__int128_t is_negative(__int128_t value);
+#endif
---
+++ C-ABI/libnegative64.h
@@ -0,0 +1,4 @@
+#ifndef IS_NEGATIVE
+#define IS_NEGATIVE
+long long is_negative(long long value);
+#endif
